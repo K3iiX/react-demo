2c743e09e9e3cb9d0512eb9787a945dfc981cc3e
K3iiX <karaya@marandino.dev>
2024-08-02 02:11:54 -0600

    setup: main components and initial layout in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero.jsx";
+import HomeCards from "./components/HomeCards.jsx";
+import JobListings from "./components/JobListings.jsx";
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
-  )
+    <>
+      <Navbar/>
+      <Hero/>
+      <HomeCards/>
+      <JobListings/>
+      
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
 };
-export default App
+export default App;
