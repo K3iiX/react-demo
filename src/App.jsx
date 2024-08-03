294f2491ae16458ed07da413352c84279f896a2b
K3iiX <karaya@marandino.dev>
2024-08-03 05:18:03 -0600

    feat: Set up routing and add Link component with react-router-dom also

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero.jsx";
-import HomeCards from "./components/HomeCards.jsx";
-import JobListings from "./components/JobListings.jsx";
-import ViewAllJobs from "./components/ViewAllJobs.jsx";
+import { 
+  Route, 
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom';
+
+import MainLayout from './layouts/MainLayout';
+import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout/>}>
+    <Route index element={<HomePage/>} />
+    <Route path='/jobs' element={<JobsPage/>} />
+  </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar/>
-      <Hero/>
-      <HomeCards/>
-      <JobListings/>
-      <ViewAllJobs/>
-    </>
-  );
+  return <RouterProvider router={router}/>;
+
 };
 export default App;
